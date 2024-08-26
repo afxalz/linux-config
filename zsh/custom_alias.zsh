0b7810baa6403ec58864e9e0f665a41e534533f8
Afzal Ahmad <afzalhmd14@gmail.com>
2024-08-26 10:38:11 +0200

    added zsh config

---
+++ zsh/custom_alias.zsh
@@ -0,0 +1,1 @@
+alias flog="~/.scripts/git-forest.sh --all --date=relative --abbrev-commit --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --style=15"
