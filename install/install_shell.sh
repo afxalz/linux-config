e4af14d3b01a614c7109b821fc11c35141dd08d4
Afzal Ahmad <afzalhmd14@gmail.com>
2026-05-13 11:12:49 +0200

    added: conda install

--- install/install_shell.sh
+++ install/install_shell.sh
@@ -3,6 +3,9 @@ set -e
 
 PATH_SCRIPT=$(dirname "$0")
 
+param_toilet_small="-f mini"
+param_toilet_big="-f future"
+
 echo "Installing: Shell tools"
 
 sudo apt-get update -y
@@ -15,42 +18,51 @@ sudo apt-get install -y \
   bat
 
 if fc-list | grep -qi "FiraCode"; then
-  toilet -w 20 -f future "Skipping: Nerd font FiraCode (already installed)"
+  toilet "$param_toilet_small" "Skipping: Nerd font FiraCode (already installed)"
 else
-  toilet -w 200 -f future "Installing: Nerd font FiraCode"
+  toilet "$param_toilet_big" "Installing: Nerd font FiraCode"
   mkdir -p "$HOME"/.local/share/fonts
   cd "$HOME"/.local/share/fonts
   curl -fLO https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip
   unzip FiraCode.zip
   rm FiraCode.zip
   fc-cache -fv
+  cd $PATH_SCRIPT
 fi
 
-cd $PATH_SCRIPT
-
-toilet -w 200 -f future "Installing: Terminal emulators"
+toilet "$param_toilet_big" "Installing: Terminal emulators"
 sudo apt-get install -y kitty alacritty
 
-toilet -w 200 -f future "Installing: Tmux terminal multiplexer"
+toilet "$param_toilet_big" "Installing: Tmux terminal multiplexer"
 sudo apt-get install -y tmux
 
-toilet -w 200 -f future "Installing: Zsh advanced shell and its plugins"
+if command -v conda &>/dev/null; then
+  toilet "$param_toilet_big" "Installing: Anaconda for python"
+  cd /tmp
+  curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+  sha256sum Miniconda3-latest-Linux-x86_64.sh
+  ./Miniconda3-latest-Linux-x86_64.sh
+  cd $PATH_SCRIPT
+else
+  toilet "$param_toilet_small" "Skipping: Conda"
+fi
+
+toilet "$param_toilet_big" "Installing: Zsh advanced shell and its plugins"
 sudo apt-get install -y zsh
 if [ -d "$HOME/.oh-my-zsh" ]; then
-  toilet -w 200 -f future "Skipping: Oh-My-Zsh (already installed)"
+  toilet "$param_toilet_small" "Skipping: Oh-My-Zsh (already installed)"
 else
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
-
 # setup zsh as default shell
 if [ "$SHELL" != "$(which zsh)" ]; then
   chsh -s $(which zsh)
 fi
 
 if ! command -v starship &>/dev/null; then
-  toilet -w 200 -f future "Installing: Starship shell prompt"
+  toilet "$param_toilet_big" "Installing: Starship shell prompt"
   curl -sS https://starship.rs/install.sh | sh
 else
-  toilet -w 200 -f future "Skipping: Starship (already installed)"
+  toilet "$param_toilet_small" "Skipping: Starship (already installed)"
 fi
 exit 0
